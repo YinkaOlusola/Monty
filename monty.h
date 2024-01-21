bcacb76e13701971c1dbac9d6da6c8bdf4391727
YinkaOlusola <pastorcooler@gmail.com>
2024-01-21 03:32:59 -0800

    Performaining no operation

--- monty.h
+++ monty.h
@@ -78,6 +78,9 @@ void f_pint(stack_t **head, unsigned int counter);
 void f_pop(stack_t **head, unsigned int counter);
 void f_swap(stack_t **head, unsigned int counter);
 void f_add(stack_t **head, unsigned int counter);
+void f_nop(stack_t **head, unsigned int counter);
+
+
 
 
 
