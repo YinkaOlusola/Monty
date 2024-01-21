bc7daf431304b0251b239f3bd94e14cd9e44031e
YinkaOlusola <pastorcooler@gmail.com>
2024-01-20 17:56:03 -0800

    Removing an element from the stack

--- stack_operations.c
+++ stack_operations.c
@@ -1,4 +1,7 @@
 #include "monty.h"
+
+/**** PRINTING THE ELEMENT AT THE TOP OF THE STACK ****/
+
 /**
  * f_pint - prints the top
  * @head: stack head
@@ -16,4 +19,30 @@ void f_pint(stack_t **head, unsigned int counter)
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*head)->n);
+}
+
+
+/****** POPPING AN ELEMENT FROM THE STACK ******/
+
+/**
+ * f_pop - prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_pop(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	*head = h->next;
+	free(h);
 }
